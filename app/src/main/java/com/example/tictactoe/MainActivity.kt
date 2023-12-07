a679e2565d0d6e7f74b30bc2462df829e355a6c3
tjmtic <tjmcard@gmail.com>
2023-12-07 02:33:08 -0800

    Update MainActivity.kt
    Test

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -73,7 +73,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello edit $name!",
         modifier = modifier
     )
 }
